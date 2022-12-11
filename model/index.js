cbfdfedaa000e1afeb2fd5c5b42fb6f4a5e45bec
unknown <jcfisher13579@gmail.com>
2022-12-11 12:26:55 -0500

    added mainpage route, get one movie, and get one review routes

--- model/index.js
+++ model/index.js
@@ -1,6 +1,13 @@
 const Users = require('./Users');
-const Movie = require('./movieModel');
+const Movie = require('./Movie');
+const Review = require ('./Review');
 
+Movie.hasOne(Review, {
+    foreignKey: 'movie_rank'
+});
 
+Review.belongsTo(Movie, {
+    foreignKey: 'movie_rank'
+});
 
-module.exports = { Users, Movie };
+module.exports = { Users, Movie, Review};
