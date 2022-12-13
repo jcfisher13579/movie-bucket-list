0b3ff68d447967f949a86dce1678f5b242c8c627
rachel howard <rdfields13@gmail.com>
2022-12-13 13:11:59 -0600

    got login routes to work

--- public/js/login.js
+++ public/js/login.js
@@ -8,7 +8,7 @@ const loginFormHandler = async (event) => {
 
   if (email && password) {
     // Send a POST request to the API endpoint
-    const response = await fetch("/api/users/login", {
+    const response = await fetch("/api/user/login", {
       method: "POST",
       body: JSON.stringify({ email, password }),
       headers: { "Content-Type": "application/json" },
@@ -31,7 +31,7 @@ const signupFormHandler = async (event) => {
   const password = document.querySelector("#password-signup").value.trim();
 
   if (userName && email && password) {
-    const response = await fetch("/api/user", {
+    const response = await fetch("/api/user/signup", {
       method: "POST",
       body: JSON.stringify({ userName, email, password }),
       headers: { "Content-Type": "application/json" },
